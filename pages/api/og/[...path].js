e22d84fc92613095ad7e6c5fd1a89d56aad5b7fc
tdkn <web@tedokon.com>
2021-01-25 16:41:15 +0900

    Update

---
+++ pages/api/og/[...path].js
@@ -0,0 +1,66 @@
+import * as playwright from "playwright-aws-lambda";
+import React from "react";
+import path from "path";
+import fs from "fs";
+import ReactDOMServer from "react-dom/server";
+import Template from "~/scripts/template";
+import { postFiles } from "~/lib/mdx";
+import matter from "gray-matter";
+
+const isDev = process.env.NODE_ENV !== "production";
+const isPreview = typeof process.env.BLOG_PREVIEW !== "undefined";
+
+async function getLaunchOptions() {
+  if (isDev || isPreview) {
+    return {
+      args: [],
+      executablePath:
+        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      headless: true,
+    };
+  } else {
+    return {};
+  }
+}
+
+function getHtml({ title }) {
+  const doctype = `<!doctype html>`;
+  const fontPath = path.resolve(
+    process.cwd(),
+    "./scripts/MPLUSRounded1c-Bold.ttf"
+  );
+  const font = fs.readFileSync(fontPath, { encoding: "base64" });
+  const element = React.createElement(Template, { title, font });
+  const markup = ReactDOMServer.renderToStaticMarkup(element);
+
+  return `${doctype}${markup}`;
+}
+
+export default async (req, res) => {
+  const { query } = req;
+  const [year, slug] = query.path;
+  const postFile = postFiles.find(file => file.year === year && file.slug === slug);
+  const source = fs.readFileSync(postFile.absolutePath);
+  const { data: frontMatter } = matter(source);
+  const options = await getLaunchOptions();
+  const browser = await playwright.launchChromium(options);
+  const page = await browser.newPage({
+    viewport: {
+      width: 1200,
+      height: 630,
+    },
+  });
+  const html = getHtml({ title: frontMatter.title });
+
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
+  await page.evaluateHandle("document.fonts.ready");
+
+  const data = await page.screenshot({ type: "png" });
+  await browser.close();
+
+  // Set the s-maxage property which caches the images then on the Vercel edge
+  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
+  res.setHeader("Content-Type", "image/png");
+  // write the image to the response with the specified Content-Type
+  res.end(data);
+};
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import matter from "gray-matter";
 import React from "react";
+import { DateTime } from "luxon";
 import { postFiles } from "~/lib/mdx";
 import MainLayout from "~/components/layouts/MainLayout";
 import PostCard from "~/components/PostCard";
@@ -16,18 +17,19 @@ export default function Index({ posts }) {
 }
 
 export const getStaticProps = () => {
-  const posts = postFiles.map(({ absolutePath, year, slug }) => {
-    const source = fs.readFileSync(absolutePath);
-    const { data } = matter(source);
-    const frontMatter = JSON.parse(JSON.stringify(data));
+  const posts = postFiles
+    .map(({ absolutePath, year, slug }) => {
+      const source = fs.readFileSync(absolutePath);
+      const { data: frontMatter } = matter(source);
 
-    return {
-      url: `/${year}/${slug}`,
-      title: frontMatter.title,
-      date: frontMatter.date,
-      summary: frontMatter.summary,
-    };
-  });
+      return {
+        url: `/${year}/${slug}`,
+        title: frontMatter.title,
+        date: frontMatter.date,
+        summary: frontMatter.summary,
+      };
+    })
+    .sort((a, b) => DateTime.fromISO(b.date) - DateTime.fromISO(a.date));
 
   return { props: { posts } };
 };
