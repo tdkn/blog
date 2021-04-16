63f32d6656dab76e7cdc4770ef4b04e3dc020b72
tdkn <web@tedokon.com>
2021-04-16 13:30:22 +0900

    fix: eslint error

--- lib/format-date.ts
+++ lib/format-date.ts
@@ -1,9 +1,7 @@
 import { parseISO, format, formatDistance } from "date-fns";
 
-export const formatDate = (
-  dateStr: string,
-  formatStr: string = "MMMM d, yyyy"
-) => format(parseISO(dateStr), formatStr);
+export const formatDate = (dateStr: string, formatStr = "MMMM d, yyyy") =>
+  format(parseISO(dateStr), formatStr);
 
 export const formatTimeAgo = (dateStr: string) =>
   formatDistance(parseISO(dateStr), new Date(), { addSuffix: true });
