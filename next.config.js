8c3569b7ee3c3aa58bf457498231fbaf2ed69cd4
Shun Tedokon <shun@tedokon.com>
2021-08-16 03:04:29 +0900

    feat: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = () => {
-  return {
-    poweredByHeader: false,
-  };
-};
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  poweredByHeader: false,
+});
