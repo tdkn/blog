864035a86923754941c544ac12cb2bf1a794e2ec
Shun Tedokon <shun@tedokon.com>
2022-05-22 03:58:30 +0900

    fix(gtag): fixed GTM ID

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default function Document() {
       <body>
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`}
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
