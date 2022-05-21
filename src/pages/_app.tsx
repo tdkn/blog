c569bae014ba770cecbb645e1e85f885d0d325b7
Shun Tedokon <shun@tedokon.com>
2022-05-22 03:28:38 +0900

    fix(ga): remove google-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { Head } from "~/components/common";
-import initGoogleAnalytics from "~/lib/google-analytics";
-
-initGoogleAnalytics();
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   // noinspection HtmlRequiredTitleElement
