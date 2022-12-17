b6620e8fd1dec0cc9428c3f61c9704326fb9164e
tdkn <me@tdkn.dev>
2022-12-18 04:22:27 +0900

    chore: cleanup deps

--- src/components/common/OpenGraphTemplate/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "./OpenGraphTemplate";
-export * from "./OpenGraphTemplate";
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,6 +3,5 @@ export { default as Footer } from "./Footer";
 export { default as GoogleTagManagerScript } from "./GoogleTagManagerScript";
 export { default as Head } from "./Head";
 export { default as Header } from "./Header";
-export { default as OpenGraphTemplate } from "./OpenGraphTemplate";
 export { default as PostCard } from "./PostCard";
 export { default as Profile } from "./Profile";
