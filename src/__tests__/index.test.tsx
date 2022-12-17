c3df237af3a0998bbbcd3f7aec9106b96b6d085c
Shun Tedokon <shun@tedokon.com>
2022-12-18 01:41:53 +0900

    chore: add snapshot test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -3,11 +3,16 @@ import { posts } from "~/lib/mdx";
 import Home from "~/pages/index";
 
 describe("Home", () => {
-  it("renders a Home", async () => {
+  it("renders a Home", () => {
     jest.spyOn(console, "error").mockImplementation();
 
     render(<Home posts={posts} />);
 
     expect(console.error).not.toHaveBeenCalled();
+  });
+  it("renders homepage unchanged", () => {
+    const { container } = render(<Home posts={posts} />);
+
+    expect(container).toMatchSnapshot();
   });
 });
