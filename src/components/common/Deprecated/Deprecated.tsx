b6275311e3bfbbd26c4cea5c7d889a876923916e
Shun Tedokon <shun@tedokon.com>
2023-01-27 02:08:08 +0900

    fix(design): changed deprecated badge round full to medium

--- src/components/common/Deprecated/Deprecated.tsx
+++ src/components/common/Deprecated/Deprecated.tsx
@@ -4,7 +4,7 @@ import { classNames } from "~/lib/style";
 const Deprecated = ({ className }: ComponentPropsWithoutRef<"span">) => (
   <span
     className={classNames(
-      "inline-flex items-center rounded-full bg-yellow-100 px-3 py-0.5 text-xs font-medium text-yellow-800 dark:bg-yellow-100/20 dark:text-orange-500",
+      "inline-flex items-center rounded-md bg-yellow-100 px-2 py-0.5 text-xs font-medium text-yellow-800 dark:bg-yellow-100/20 dark:text-orange-500",
       className
     )}
   >
