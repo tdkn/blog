975a1853da6b3aad88e9d87afb0989a6a49351a9
tdkn <me@tdkn.dev>
2023-03-08 01:03:57 +0900

    refactor: migrate to Metadata API

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,5 @@
 export { default as Deprecated } from "./Deprecated";
 export { default as Footer } from "./Footer";
-export { default as GoogleTagManagerScript } from "./GoogleTagManagerScript";
-export { default as Head } from "./Head";
 export { default as Header } from "./Header";
 export { default as PostCard } from "./PostCard";
 export { default as Profile } from "./Profile";
