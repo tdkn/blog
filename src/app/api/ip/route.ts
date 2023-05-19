a66f5bf9bd1cc14026e0d3d28fc7b8abe754b348
tdkn <me@tdkn.dev>
2023-05-20 00:17:29 +0900

    refactor: replace pages api to route handler

---
+++ src/app/api/ip/route.ts
@@ -0,0 +1,8 @@
+export async function GET(request: Request) {
+  // NOTE: `0.0.0.0` is fallback for localhost or non Vercel deployments
+  const ip = request.headers.get("x-forwarded-for") || "0.0.0.0";
+
+  return new Response(ip, {
+    status: 200,
+  });
+}
