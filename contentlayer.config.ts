e6b253a574de9dccf58153c3e45fdeccff2cf742
Shun Tedokon <shun@tedokon.com>
2023-06-30 14:57:11 +0900

    fix: syntax highlight

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -40,20 +40,6 @@ export const Post = defineDocumentType(() => ({
 
 const rehypePrettyCodeOptions: Partial<Options> = {
   theme: "dracula-soft",
-  onVisitLine(node: any) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [{ type: "text", value: " " }];
-    }
-  },
-  // Feel free to add classNames that suit your docs
-  onVisitHighlightedLine(node: any) {
-    node.properties.className.push("highlighted");
-  },
-  onVisitHighlightedWord(node: any) {
-    node.properties.className = ["word"];
-  },
 };
 
 export default makeSource({
