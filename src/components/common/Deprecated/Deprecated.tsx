3749989eede5f044d482983c9ecb8c97ad34c6ad
tdkn <me@tdkn.dev>
2023-07-28 00:08:21 +0900

    chore(styles): reformat with prettier

--- src/components/common/Deprecated/Deprecated.tsx
+++ src/components/common/Deprecated/Deprecated.tsx
@@ -5,7 +5,7 @@ const Deprecated = ({ className }: ComponentPropsWithoutRef<"span">) => (
   <span
     className={classNames(
       "inline-flex items-center rounded-md bg-yellow-100 px-2 py-0.5 text-xs font-medium text-yellow-800 dark:bg-yellow-100/20 dark:text-orange-500",
-      className
+      className,
     )}
   >
     Deprecated
