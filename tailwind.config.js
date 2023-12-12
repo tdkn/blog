92cac10f9122d4ae3a9284c1803d53460e39d7c3
Shun Tedokon <shun@tedokon.com>
2023-12-12 21:53:35 +0900

    refactor: remove unnecessary content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/pages/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-  ],
+  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
