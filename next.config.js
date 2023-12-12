2a824cf4f35cdf5f5b9a685f06dcabf00b6feffd
Shun Tedokon <shun@tedokon.com>
2023-12-12 15:34:30 +0900

    chore: add some redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withContentlayer } = require("next-contentlayer");
 
 /**
@@ -6,6 +7,35 @@ const { withContentlayer } = require("next-contentlayer");
  */
 const nextConfig = {
   poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/tdkn",
+        permanent: false,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/tdkn_",
+        permanent: false,
+      },
+      {
+        source: "/bluesky",
+        destination: "https://bsky.app/profile/tdkn.bsky.social",
+        permanent: false,
+      },
+      {
+        source: "/mastodon",
+        destination: "https://mastodon.social/@tdkn",
+        permanent: false,
+      },
+      {
+        source: "/facebook",
+        destination: "https://facebook.com/shun.tedokon",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
