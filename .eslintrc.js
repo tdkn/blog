2132c051affe45e1919acb31113d3ca3e4937f7e
Shun Tedokon <shun@tedokon.com>
2023-12-17 17:50:43 +0900

    chore: update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
-module.exports = {
+// @ts-check
+
+/** @type {import("eslint").Linter.Config} */
+const eslintConfig = {
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
   rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "react/jsx-no-useless-fragment": ["error", { allowExpressions: true }],
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": [
       "error",
       {
         argsIgnorePattern: "^_",
-      },
-    ],
-    "no-console": [
-      "error",
-      {
-        allow: ["warn", "error", "info"],
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
       },
     ],
   },
 };
+
+module.exports = eslintConfig;
