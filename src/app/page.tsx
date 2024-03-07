53df277a1887087fd639ab4e41049ecfdd5206ca
Shun Tedokon <shun@tedokon.com>
2024-03-07 23:38:23 +0900

    fix: resolved random test crashes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { compareDesc } from "date-fns";
+
 import { PostCard, Profile } from "~/components/common";
 import { getAllPosts } from "~/lib/mdx";
 
 export default async function HomePage() {
-  const posts = await getAllPosts();
+  const posts = (await getAllPosts()).sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date)),
+  );
 
   return (
     <>
